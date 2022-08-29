ddf04a8a899d299b48e65a22ce309db97593f9f3
maria-moroz <mma.moroz.mariia@gmail.com>
2022-08-29 20:40:02 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { store, persistor } from 'redux/store';
 import App from 'components/App';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
